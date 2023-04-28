e00303b9ff1eefb93ea32fec69f4fd649562a36d
Leo <leobensman@gmail.com>
2023-04-28 15:15:08 -0400

    change polymorphic language in models/sale.rb

--- app/models/payola/sale.rb
+++ app/models/payola/sale.rb
@@ -17,8 +17,10 @@ module Payola
     # belongs_to :coupon, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
     # belongs_to :affiliate, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
 
-    belongs_to :product, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
-    belongs_to :owner, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
+    # belongs_to :product, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
+    belongs_to :product, polymorphic: true, optional: true
+    # belongs_to :owner, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
+    belongs_to :owner, polymorphic: true, optional: true
     belongs_to :coupon, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
     belongs_to :affiliate, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
     
